fcb501d18bbc4dc700dc0d936e24369a44fbd6ca
bai.bsx <bai.bsx@alibaba-inc.com>
2019-10-16 17:36:56 +0800

    viewModel get LiveData

--- JetpackSample/app/src/main/java/com/bsx/jetpacksample/viewmodel/UserProfileViewModel.java
+++ JetpackSample/app/src/main/java/com/bsx/jetpacksample/viewmodel/UserProfileViewModel.java
@@ -3,6 +3,8 @@ package com.bsx.jetpacksample.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.bsx.jetpacksample.model.RepositoryFactory;
+import com.bsx.jetpacksample.model.UserRepository;
 import com.bsx.jetpacksample.model.database.entity.User;
 
 /**
@@ -11,8 +13,18 @@ import com.bsx.jetpacksample.model.database.entity.User;
  * @author baishixian
  */
 public class UserProfileViewModel extends ViewModel {
+    private final UserRepository userRepository;
     private LiveData<User> user;
 
     public UserProfileViewModel() {
+        this.userRepository = RepositoryFactory.getInstance().getUserRepository();
+    }
+
+    public LiveData<User> getUser(String userId) {
+        if (this.user == null) {
+            this.user = userRepository.getUser(userId);
+        }
+
+        return user;
     }
 }
