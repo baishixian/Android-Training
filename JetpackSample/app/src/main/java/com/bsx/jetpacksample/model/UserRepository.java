468c84b2a508b807185c100a2893cec4ef0d9159
bai.bsx <bai.bsx@alibaba-inc.com>
2019-10-16 17:38:26 +0800

    add getUser

--- JetpackSample/app/src/main/java/com/bsx/jetpacksample/model/UserRepository.java
+++ JetpackSample/app/src/main/java/com/bsx/jetpacksample/model/UserRepository.java
@@ -1,10 +1,16 @@
 package com.bsx.jetpacksample.model;
 
+import androidx.lifecycle.LiveData;
+
 import com.bsx.jetpacksample.model.database.dao.UserDao;
+import com.bsx.jetpacksample.model.database.entity.User;
 import com.bsx.jetpacksample.model.remote.Webservice;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.concurrent.Executor;
 
+import retrofit2.Response;
 
 /**
  * UserRepository
@@ -21,4 +27,42 @@ public class UserRepository {
         this.executor = executor;
         this.userDao = userDao;
     }
+
+    public LiveData<User> getUser(String userId) {
+        refreshUser(userId);
+
+        // Returns a LiveData object directly from the database.
+        return userDao.load(userId);
+    }
+
+    private void refreshUser(final String userId) {
+        // Runs in a background thread.
+        executor.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                // Check if user data was fetched recently.
+                boolean userExists = userDao.hasUser(userId, new Date()) >= 1;
+
+                if (userExists) {
+                    // Refreshes the data.
+                    try {
+                        Response<User> response = webservice.getUser(userId).execute();
+
+                        // Check for errors here.
+                        if (response.isSuccessful()) {
+
+                            // Updates the database. The LiveData object automatically
+                            // refreshes, so we don't need to do anything else here.
+                            userDao.save(response.body());
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
 }
